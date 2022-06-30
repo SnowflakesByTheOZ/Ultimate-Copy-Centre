a705faaa35d7014f4bae3c64aa77d5daafaf0fc1
IEXCHANGE <raymondhowell64@gmail.com>
2022-06-29 22:18:20 -0400

    Team Page / Mission Statement Page and dummy products

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 class MyHeader extends HTMLElement{
     connectedCallback (){
         this.innerHTML = `
-        <h1 id="title-text">Chase Copy Centre</h1>
+        <h1 id="title-text"><a href="index.html">Chase Copy Centre</a></h1>
         <ul id="top-menu">
             <li><a href="#">Stationery</a></li>
             <li><a href="#">Laminating Services</a></li>
             <li><a href="#">Photocopying Services</a></li>
             <li id="about-us"><i class="fa-solid fa-caret-down"></i><a  href="#">About Us</a>
                 <ul id="about-us-list">
-                    <li><a href="meetstaff.html">Meet The Staff</a></li>
-                    <li><a href="#">Mission Statement</a></li>
+                    <li><a href="meetstaff.html">Meet Our Team</a></li>
+                    <li><a href="mission.html">Mission Statement</a></li>
                     <li><a href="#">How We Got Started</a></li>
                 </ul>
             </li>
@@ -21,3 +21,13 @@ class MyHeader extends HTMLElement{
 }
 
 customElements.define('my-header', MyHeader);
+
+class MyFooter extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = `
+        <p>Built by Raymond Howell 2022</p>
+        `
+    }
+}
+
+customElements.define('my-footer',MyFooter);

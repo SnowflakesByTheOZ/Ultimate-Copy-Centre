c75c23480be4d94c111ae838cc904fc06c503e84
IEXCHANGE <raymondhowell64@gmail.com>
2022-07-09 04:46:46 -0400

    Changes

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@ class MyHeader extends HTMLElement{
     connectedCallback (){
         this.innerHTML = `
         
-        <h1 id="title-text"><a href="index.html">Ultimate Copy Centre</a></h1>
+        <h1 id="title-text"><a href="/index.html">Ultimate Copy Centre</a></h1>
         <ul id="top-menu">
             <li><a href="#">Stationery</a></li>
-            <li><a href="laminate.html">Laminating Services</a></li>
+            <li><a href="/laminate.html">Laminating Services</a></li>
             <li><a href="#">Photocopying Services</a></li>
             <li id="about-us"><i class="fa-solid fa-caret-down"></i><a  href="#">About Us</a>
                 <ul id="about-us-list">
-                    <li><a href="meetstaff.html">Meet Our Team</a></li>
-                    <li><a href="mission.html">Mission Statement</a></li>
+                    <li><a href="/meetstaff.html">Meet Our Team</a></li>
+                    <li><a href="/mission.html">Mission Statement</a></li>
                     <li><a href="#">How We Got Started</a></li>
                 </ul>
             </li>
